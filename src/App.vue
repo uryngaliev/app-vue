5441df60c1eb970282444dce94f86c0d3ce668b4
uryngaliev <zhainar999@gmail.com>
2022-08-26 17:26:45 +0500

    create an add post form

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-    <PostList :posts="posts"/>
+    <div class="container mx-auto px-4 mt-4">
+        <form @submit.prevent>
+            <input v-model="title" type="text" placeholder="Название">
+            <input v-model="body" type="text" placeholder="Описание">
+            <button @click="createPost">
+                Создать запись
+            </button>
+        </form>
+        <PostList :posts="posts"/>
+    </div>
 </template>
 
 <script>
@@ -13,8 +22,24 @@
                     {title: 'Vue', body: '3'},
                     {title: 'Vue', body: '3'},
                     {title: 'Vue', body: '3'},
-                ]
+                ],
+                title: '',
+                body: '',
             }
+        },
+
+        methods: {
+            createPost() {
+                const newPost = {
+                    id: Date.now(),
+                    title: this.title,
+                    body: this.body
+                }
+                this.posts.push(newPost);
+                this.title = '';
+                this.body = '';
+            }
+
         }
     }
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mx-auto px-4 mt-4" v-for="post in posts">
+    <div v-for="post in posts">
         <div class="flex border border-red-500 py-2 px-4">
             <h2 class="text-xl tracking-normal">
                 {{post.title}}
