95270cbcc7cab71644b95a99769867fab5b86ae3
uryngaliev <zhainar999@gmail.com>
2022-09-01 12:14:14 +0500

    add modal window and update post add form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="container mx-auto px-4 mt-4">
-        <PostForm @create="createPost"/>
-        <PostList  :posts="posts"/>
+        <h1>Страница постов</h1>
+        <button @click="showDialog">Создать пост</button>
+        <my-dialogs v-model:show="dialogVisible">
+            <PostForm @create="createPost"/>
+        </my-dialogs>
+        <PostList :posts="posts"/>
     </div>
 </template>
 
 <script>
     import PostList from "@/components/PostList";
     import PostForm from "@/components/PostForm";
+    import MyDialogs from "@/components/MyDialogs";
 
     export default {
-        components: {PostForm, PostList},
+        components: {MyDialogs, PostForm, PostList},
         data() {
             return {
                 posts: [
@@ -18,14 +23,17 @@
                     {title: 'Vue', body: '3'},
                     {title: 'Vue', body: '3'},
                 ],
-                title: '',
-                body: '',
+                dialogVisible: false,
             }
         },
 
         methods: {
             createPost(post) {
                 this.posts.push(post)
+                this.dialogVisible = false
+            },
+            showDialog() {
+                this.dialogVisible = true
             }
 
         }
---
+++ src/components/MyDialogs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="dialog bg-black opacity-25 fixed inset-0 flex justify-center" v-if="show" @click="hideDialog">
+        <div @click.stop class="dialog_content m-auto bg-white rounded-lg w-[300px] h-[200px] p-4">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyDialogs",
+        props: {
+            show: {
+                type: Boolean,
+                default: false
+            }
+        },
+        methods: {
+            hideDialog() {
+                this.$emit('update:show', false)
+            }
+        }
+    }
+</script>
